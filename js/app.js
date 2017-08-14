20818d6c2d456cb8a029db4fcb30a4678c038287
lahuke <luke@luketucker.com>
2017-08-14 11:08:43 -0600

    almost done

--- js/app.js
+++ js/app.js
@@ -7,16 +7,38 @@ $(document).ready(function() {
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
 	})
-	.mousedown(function() {
-    	 // play hadouken sound
+	.mousedown(function(){
+    	playHadouken();
     	$('.ryu-ready').hide();
     	$('.ryu-throwing').show();
-    	$('.hadouken').show();
-    	// show hadouken and animate it to the right of the screen
+    	$('.hadouken').finish().show().animate(
+  			{'left': '1020px'},
+  			500,
+  			function(){ 
+    		$(this).hide();
+    		$(this).css('left', '520px');
+ 			});
   })
-  .mouseup(function() {
+  	.mouseup(function() {
     	$('.ryu-throwing').hide();
     	$('.ryu-ready').show();
-    // ryu goes back to his ready position
   });
+
+  $(document).keydown(function(e){
+  		if (e.keyCode == 88) {
+  		$('.ryu-ready').hide();
+  		$('.ryu-cool').show();
+  	}
+  })
+  	.keyup(function(e){
+  		if(e.keyCode == 88) {
+  		$('.ryu-cool').hide();
+  		$('.ryu-ready').show();
+  }
 });
+  });
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
